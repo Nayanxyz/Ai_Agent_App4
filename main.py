3734fdef85c76d96cf5a744d61116576337c1884
NayanPython <choudharyji527@gmail.com>
2026-03-02 13:06:37 +0530

    Final commit aap4 Ai Agent for adding tasks projects and more

--- main.py
+++ main.py
@@ -1,14 +1,45 @@
-from dotenv import load_dotenv
+from dotenv import load_dotenv                                                   #dotenv is a library to read .env folder
 
-import os
+import os                                                                        #os library is a bridge to operating system
 
-from langchain_core.messages import HumanMessage, AIMessage
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain_google_genai import ChatGoogleGenerativeAI
-from pydantic_core.core_schema import model_field
-from langchain.tools import tool
-from langchain.agents import create_openai_tools_agent , AgentExecutor
+from langchain_core.messages import HumanMessage, AIMessage                     # Langchain are nerves connecting brain and (LLM) and parts (Todoist app)
+                                                                                # messages are objects used in prompts and chat conversations.
+                                                                                #HumanMessages are messages that are passed in from a human to the model.
+                                                                                #AIMessage is returned from a chat model as a response to a prompt
+
+from langchain_core.output_parsers import StrOutputParser                      #In LangChain, an Output Parser is a dedicated class designed to take the raw,
+                                                                               # often unstructured text output from a Large Language Model (LLM) and
+                                                                               # convert it into a structured, reliable, and usable format (such as JSON, Pydantic objects, or Python lists)
+                                                                               #StrOutputParser that parses LLMResult into the top likely string.
+
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder    #prompts is the input to the model
+                                                                              #ChatPromptTemplate Use to create flexible templated prompts for chat models
+                                                                              #Prompt template that assumes variable is already list of messages.
+                                                                              #A placeholder which can be used to pass in a list of messages.
+
+from langchain_google_genai import ChatGoogleGenerativeAI                     #This module integrates Google's Generative AI models, specifically the Gemini series, with the LangChain framework.
+                                                                              # It provides classes for interacting with chat models and generating embeddings
+                                                                              #The ``ChatGoogleGenerativeAI`` class is the primary interface for interacting with Google's Gemini chat models.
+                                                                              # It allows users to send and receive messages using a specified Gemini model, suitable for various conversational AI applications.
+
+from pydantic_core.core_schema import model_field                             #pydantic_core is the underlying high-performance Rust-based validation logic engine for the Python Pydantic library.
+                                                                              #core_schema module in the Pydantic library contains the definitions used to build schemas that Pydantic's core validation and serialization logic
+                                                                              #model_field It holds all the necessary information for validating and serializing that specific field,
+                                                                              # including its type, constraints, default value, and any other metadata
+
+from langchain.tools import tool                                              #tool are classes that an Agent uses to interact with the world.Each tool has a **description**.
+                                                                              # Agent uses the description to choose the right tool for the job.
+                                                                              #Make tools out of functions, can be used with or without arguments
+
+from langchain.agents import create_openai_tools_agent , AgentExecutor        #Agents is a class that uses an LLM to choose a sequence of actions to take
+                                                                              #In Chains, a sequence of actions is hardcoded. In Agents,a language model is used as a reasoning engine
+                                                                              # to determine which actions to take and in which order
+                                                                              #create_openai_tools_agent create an agent that uses OpenAI tools (gets three arguments)
+                                                                              #llm: LLM to use as the agent.
+                                                                              #tools: Tools this agent has access to.
+                                                                              # prompt: The prompt to use. See Prompt section below for more on the expected input variables
+                                                                              #AgentExecutor Agent that is using tools.
+
 from todoist_api_python.api import TodoistAPI
 
 
@@ -53,7 +84,7 @@ prompt = ChatPromptTemplate([
     MessagesPlaceholder("agent_scratchpad")
 ])
 
-#chain = prompt | llm | StrOutputParser()
+#chain = prompt | llm | StrOutputParser()                                                 #prompt inside llm converts String output and annalize  Chain method
 agent = create_openai_tools_agent(llm, tools , prompt)
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=False)
 
